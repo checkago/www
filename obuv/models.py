f0d8bbe590b6170c557c026167a1d7d8da9dea09
CHECKago <mfalcon@mail.ru>
2020-04-08 21:19:41 +0300

    1

--- obuv/models.py
+++ obuv/models.py
@@ -5,3 +5,7 @@ class Banner(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     anons = models.TextField(verbose_name='Анонс')
+    image = models.ImageField(upload_to='media/banners')
+    def meta(self):
+        verbose_name = 'Баннер'
+        verbose_name_plural = 'Баннеры'
--- obuv/views.py
+++ obuv/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Banner
+
 
 def index(request):
+    banners = Banner.objects.all()
     title = 'Производство обуви'
     description = ''
-    return render(request, 'index.html', title)
+    return render(request, 'index.html', {title:'title', banners:'banners', description:'description'})
