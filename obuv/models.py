75e96aca5791ac65cd12c68608df3147d3450a6e
CHECKago <mfalcon@mail.ru>
2020-04-08 19:46:57 +0300

    1

--- obuv/models.py
+++ obuv/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Banner(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    anons = models.TextField(verbose_name='Анонс')
